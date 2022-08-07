17c5b3b203b024778aac5a4d4e5c24dd2acaa6a0
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-08-07 17:15:11 -0300

    cria novo usuario

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todo: []
+  }
+
+  users.push(user);
+  return response.status(201).json(user);
 });
+
+app.get("/users", (request, response) => {
+  response.status(200).json(users);
+})
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
